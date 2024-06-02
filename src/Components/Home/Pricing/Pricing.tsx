ab967efe751286d70d758029cf55219b911ea4c9
fahima <sfahima1388@gmail.com>
2024-06-03 00:03:10 +0600

    added toggle menu button for smaller device

--- src/Components/Home/Pricing/Pricing.tsx
+++ src/Components/Home/Pricing/Pricing.tsx
@@ -1,6 +1,6 @@
 const Pricing = () => {
   return (
-    <div className="mx-auto max-w-3xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 dark:text-white">
+    <div className="mx-auto max-w-3xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
       <div className="md:max-w-md mx-auto text-center mb-5">
         <h6 className="uppercase md:text-lg font-medium tracking-[6px]">
           pricing
@@ -10,19 +10,20 @@ const Pricing = () => {
         </h1>
       </div>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:items-center md:gap-8">
-        <div className="rounded-2xl border border-[#FF5555] p-6 shadow-sm ring-1 ring-[#FF5555] sm:order-last sm:px-8 lg:p-12">
+        <div className="rounded-2xl border border-[#FF5555] p-6 shadow-sm ring-1 ring-[#FF5555] sm:order-last sm:px-8 lg:p-12 ">
           <div className="text-center">
-            <h2 className="text-lg font-medium text-gray-900">
+            <h2 className="text-lg font-medium dark:text-white text-gray-900 dark:text-white">
               Pro
               <span className="sr-only">Plan</span>
             </h2>
             <p className="mt-2 sm:mt-4">
-              <strong className="text-3xl font-bold text-gray-900 sm:text-4xl">
+              <strong className="text-3xl font-bold dark:text-white text-gray-900 dark:text-white sm:text-4xl">
                 {" "}
                 30${" "}
               </strong>
-
-              <span className="text-sm font-medium text-gray-700">/month</span>
+              <span className="text-sm font-medium dark:text-white text-gray-700 dark:text-white">
+                /month
+              </span>
             </p>
           </div>
 
@@ -43,7 +44,10 @@ const Pricing = () => {
                 />
               </svg>
 
-              <span className="text-gray-700"> 20 users included </span>
+              <span className="dark:text-white text-gray-700">
+                {" "}
+                20 users included{" "}
+              </span>
             </li>
 
             <li className="flex items-center gap-1">
@@ -62,7 +66,10 @@ const Pricing = () => {
                 />
               </svg>
 
-              <span className="text-gray-700"> 5GB of storage </span>
+              <span className="dark:text-white text-gray-700">
+                {" "}
+                5GB of storage{" "}
+              </span>
             </li>
 
             <li className="flex items-center gap-1">
@@ -81,7 +88,10 @@ const Pricing = () => {
                 />
               </svg>
 
-              <span className="text-gray-700"> Email support </span>
+              <span className="dark:text-white text-gray-700">
+                {" "}
+                Email support{" "}
+              </span>
             </li>
 
             <li className="flex items-center gap-1">
@@ -100,7 +110,10 @@ const Pricing = () => {
                 />
               </svg>
 
-              <span className="text-gray-700"> Help center access </span>
+              <span className="dark:text-white text-gray-700">
+                {" "}
+                Help center access{" "}
+              </span>
             </li>
 
             <li className="flex items-center gap-1">
@@ -119,7 +132,10 @@ const Pricing = () => {
                 />
               </svg>
 
-              <span className="text-gray-700"> Phone support </span>
+              <span className="dark:text-white text-gray-700">
+                {" "}
+                Phone support{" "}
+              </span>
             </li>
 
             <li className="flex items-center gap-1">
@@ -137,27 +153,32 @@ const Pricing = () => {
                   d="M4.5 12.75l6 6 9-13.5"
                 />
               </svg>
-              <span className="text-gray-700"> Community access </span>
+              <span className="dark:text-white text-gray-700">
+                {" "}
+                Community access{" "}
+              </span>
             </li>
           </ul>
-          <button className="mt-8 block rounded-full border border-[#FF5555] bg-[#FF5555] px-12 py-3 text-center text-sm font-medium text-white hover:bg-black hover:ring-1 hover:ring-black focus:outline-none focus:ring ">
+          <button className="mt-8 block rounded-full border border-[#FF5555] bg-[#FF5555] px-12 py-3 text-center text-sm font-medium text-white hover:bg-black hover:ring-1 hover:ring-black focus:outline-none focus:ring transition ease-linear duration-300 dark:hover:bg-white dark:hover:text-[#FF5555]">
             Get Started
           </button>
         </div>
         <div className="rounded-2xl border border-gray-200 p-6 shadow-sm sm:px-8 lg:p-12">
           <div className="text-center">
-            <h2 className="text-lg font-medium text-gray-900">
+            <h2 className="text-lg font-medium dark:text-white text-gray-900">
               Starter
               <span className="sr-only">Plan</span>
             </h2>
 
             <p className="mt-2 sm:mt-4">
-              <strong className="text-3xl font-bold text-gray-900 sm:text-4xl">
+              <strong className="text-3xl font-bold dark:text-white text-gray-900 sm:text-4xl">
                 {" "}
                 20${" "}
               </strong>
 
-              <span className="text-sm font-medium text-gray-700">/month</span>
+              <span className="text-sm font-medium dark:text-white text-gray-700">
+                /month
+              </span>
             </p>
           </div>
 
@@ -178,7 +199,10 @@ const Pricing = () => {
                 />
               </svg>
 
-              <span className="text-gray-700"> 10 users included </span>
+              <span className="dark:text-white text-gray-700">
+                {" "}
+                10 users included{" "}
+              </span>
             </li>
 
             <li className="flex items-center gap-1">
@@ -197,7 +221,10 @@ const Pricing = () => {
                 />
               </svg>
 
-              <span className="text-gray-700"> 2GB of storage </span>
+              <span className="dark:text-white text-gray-700">
+                {" "}
+                2GB of storage{" "}
+              </span>
             </li>
 
             <li className="flex items-center gap-1">
@@ -216,7 +243,10 @@ const Pricing = () => {
                 />
               </svg>
 
-              <span className="text-gray-700"> Email support </span>
+              <span className="dark:text-white text-gray-700">
+                {" "}
+                Email support{" "}
+              </span>
             </li>
 
             <li className="flex items-center gap-1">
@@ -234,11 +264,13 @@ const Pricing = () => {
                   d="M4.5 12.75l6 6 9-13.5"
                 />
               </svg>
-              <span className="text-gray-700">Help center access</span>
+              <span className="dark:text-white text-gray-700">
+                Help center access
+              </span>
             </li>
           </ul>
 
-          <button className="mt-8 block rounded-full border border-[#FF5555] bg-white px-12 py-3 text-center text-sm font-medium text-[#FF5555] hover:ring-1 hover:ring-black active:text-[#FF5555]">
+          <button className="mt-8 block rounded-full border border-[#FF5555] bg-white px-12 py-3 text-center text-sm font-medium text-[#FF5555] hover:ring-1 hover:ring-black active:text-[#FF5555] transition ease-linear duration-300 hover:bg-[#FF5555] hover:text-white">
             Get Started
           </button>
         </div>
--- src/Components/Shared/Navbar/NavBar.tsx
+++ src/Components/Shared/Navbar/NavBar.tsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 //importing images
 import logo from "@/app/assets/images/logo.png";
 import ThemeButton from "../Theme Button/ThemeButton";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
   const [toggleNav, setToggleNav] = useState(false);
@@ -12,26 +13,42 @@ const NavBar = () => {
     setToggleNav(!toggleNav);
   };
 
+  //active
+  const path = usePathname().startsWith("href");
+
   const navLinks = (
     <>
-      <a href="#home" className=" font-bold text-xl active:text-[#FF5555]">
-        Home
-      </a>
-      <a href="#feature" className="font-medium text-xl">
-        Features
-      </a>
-      <a href="#advantage" className="font-medium text-xl">
-        Advantages
-      </a>
-      <a href="#testimonial" className="font-medium text-xl">
-        Testimonial
-      </a>
-      <a href="#pricing" className="font-medium text-xl">
-        Pricing
-      </a>
-      <button className="text-white bg-black px-7 py-3 capitalize text-xs hover:bg-[#FF5555] transform ease-linear duration-300 md:hidden">
-        download
-      </button>
+      <li>
+        <a href="#home" className=" font-bold text-xl active:text-[#FF5555]">
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="#feature" className="font-medium text-xl">
+          Features
+        </a>
+      </li>
+      <li>
+        <a href="#advantage" className="font-medium text-xl">
+          Advantages
+        </a>
+      </li>
+
+      <li>
+        <a href="#testimonial" className="font-medium text-xl">
+          Testimonial
+        </a>
+      </li>
+      <li>
+        <a href="#pricing" className="font-medium text-xl">
+          Pricing
+        </a>
+      </li>
+      <li>
+        <button className="text-white bg-black px-7 py-3 capitalize text-xs hover:bg-[#FF5555] transform ease-linear duration-300 md:hidden">
+          download
+        </button>
+      </li>
     </>
   );
   return (
@@ -45,7 +62,7 @@ const NavBar = () => {
             onClick={handleToggle}
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           >
-            {/* <span className="sr-only">Open main menu</span> */}
+            <span className="sr-only">Open main menu</span>
             <svg
               className="w-5 h-5"
               aria-hidden="true"
@@ -63,17 +80,20 @@ const NavBar = () => {
             </svg>
           </div>
         </div>
-        <div className="justify-center items-center"></div>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col justify-center items-center p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <div className="hidden w-full md:block md:w-auto">
+          <ul className="font-medium flex md:flex-row flex-col justify-center items-center p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             {navLinks}
           </ul>
         </div>
-        {/* <div className="flex gap-2 justify-center items-center"> */}
         <button className="text-white bg-black px-7 py-3 capitalize text-xs hover:bg-[#FF5555] transform ease-linear duration-300 hidden md:flex">
           download
         </button>
       </div>
+      {toggleNav && (
+        <div className="md:hidden">
+          <ul className="shadow-lg w-fit dark:text-white">{navLinks}</ul>
+        </div>
+      )}
     </nav>
   );
 };
